005072d7cb9d730e18c9ff45ddaf657a7a2fdcd9
KeylerP <Keylerporto7@gmail.com>
2026-05-28 21:10:33 -0500

    Protocoloborativo3parte

---
+++ DescuentoFijo.java
@@ -0,0 +1,12 @@
+public class DescuentoFijo extends Descuento {
+    private double montoFijo;
+
+    public DescuentoFijo(double montoFijo) {
+        this.montoFijo = montoFijo;
+    }
+
+    @Override
+    public double aplicarDescuento(double precio) {
+        return precio - montoFijo;
+    }
+}
---
+++ DocumentoWord.java
@@ -0,0 +1,6 @@
+public class DocumentoWord extends Documento {
+    @Override
+    public void exportar() {
+        System.out.println("Exportando documento a Word...");
+    }
+}
